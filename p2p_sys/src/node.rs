b968b1ef148ac439df755e4d53c88e7d259f2c22
jinzhouh <zhouhan5856@outlook.com>
2024-12-03 15:52:12 -0400

    Add files via upload

--- p2p_sys/src/node.rs
+++ p2p_sys/src/node.rs
@@ -1,6 +1,8 @@
 use futures::stream::StreamExt;
 use libp2p::{
-    gossipsub, mdns, noise, swarm::NetworkBehaviour, swarm::SwarmEvent, tcp, yamux, PeerId,
+    gossipsub, mdns,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux, PeerId,
 };
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
@@ -16,14 +18,15 @@ pub struct MyBehaviour {
     pub mdns: mdns::tokio::Behaviour,
 }
 
-pub async fn run_peer_to_peer_system(topic_name: String) -> Result<(), Box<dyn Error>> {
+pub async fn run_peer_to_peer_system(
+    topic_name: String,
+    password: String,
+) -> Result<(), Box<dyn Error>> {
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
-        .with_tcp(
-            tcp::Config::default(),
-            noise::Config::new,
-            yamux::Config::default,
-        )?
+        .with_tcp(tcp::Config::default(), libp2p::noise::Config::new, || {
+            yamux::Config::default()
+        })?
         .with_quic()
         .with_behaviour(|key| {
             let local_peer_id = key.public().to_peer_id();
@@ -46,8 +49,7 @@ pub async fn run_peer_to_peer_system(topic_name: String) -> Result<(), Box<dyn E
                 gossipsub_config,
             )?;
 
-            let mdns =
-                mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id.clone())?;
+            let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id.clone())?;
             Ok(MyBehaviour { gossipsub, mdns })
         })?
         .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
@@ -64,7 +66,10 @@ pub async fn run_peer_to_peer_system(topic_name: String) -> Result<(), Box<dyn E
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    println!("Listening for peers and publishing to topic: {}", topic_name);
+    println!(
+        "Listening for peers and publishing to topic: {}",
+        topic_name
+    );
 
     loop {
         select! {
@@ -72,7 +77,7 @@ pub async fn run_peer_to_peer_system(topic_name: String) -> Result<(), Box<dyn E
             line = stdin.next_line() => {
                 match line {
                     Ok(Some(input)) => {
-                        handle_user_input(&input, &mut swarm, &topic, &local_peer_id).await?;
+                        handle_user_input(&input, &mut swarm, &topic, &local_peer_id, &password).await?;
                     },
                     Ok(None) => break Ok(()),
                     Err(e) => {
@@ -83,7 +88,7 @@ pub async fn run_peer_to_peer_system(topic_name: String) -> Result<(), Box<dyn E
 
             // Handle swarm events
             event = swarm.select_next_some() => {
-                handle_swarm_event(event, &mut swarm).await?;
+                handle_swarm_event(event, &mut swarm, &password).await?;
             }
         }
     }
@@ -95,6 +100,7 @@ async fn handle_user_input(
     swarm: &mut libp2p::Swarm<MyBehaviour>,
     topic: &gossipsub::IdentTopic,
     local_peer_id: &PeerId,
+    password: &str,
 ) -> Result<(), Box<dyn Error>> {
     if input.trim() == "@upload" {
         // Call the function to select and broadcast a file
@@ -108,9 +114,10 @@ async fn handle_user_input(
                         .unwrap_or("unknown")
                         .to_string();
 
-                    // Create a FileMessage
+                    // Create a FileMessage with the password
                     let file_message = FileMessage {
                         sender: (*local_peer_id).to_string(),
+                        password: password.to_string(),
                         file_name,
                         data: file_data,
                     };
@@ -128,18 +135,19 @@ async fn handle_user_input(
                     } else {
                         println!("File broadcasted to peers.");
                     }
-                },
+                }
                 Err(e) => println!("Failed to read file: {}", e),
             }
         } else {
             println!("No file selected for upload.");
         }
     } else {
-        // Regular text message
+        // Regular text message with password prepended
+        let message_with_password = format!("{}:{}", password, input);
         if let Err(e) = swarm
             .behaviour_mut()
             .gossipsub
-            .publish(topic.clone(), input.as_bytes())
+            .publish(topic.clone(), message_with_password.as_bytes())
         {
             println!("Publish error: {:?}", e);
         }
@@ -151,6 +159,7 @@ async fn handle_user_input(
 async fn handle_swarm_event(
     event: SwarmEvent<MyBehaviourEvent>,
     swarm: &mut libp2p::Swarm<MyBehaviour>,
+    password: &str,
 ) -> Result<(), Box<dyn Error>> {
     match event {
         SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
@@ -158,20 +167,23 @@ async fn handle_swarm_event(
                 println!("mDNS discovered a new peer: {}", peer_id);
                 swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
             }
-        },
+        }
         SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
             for (peer_id, _) in list {
                 println!("mDNS discovered peer has expired: {}", peer_id);
-                swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                swarm
+                    .behaviour_mut()
+                    .gossipsub
+                    .remove_explicit_peer(&peer_id);
             }
-        },
+        }
         SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Message {
             propagation_source: peer_id,
             message_id: id,
             message,
         })) => {
-            handle_received_message(&peer_id, &id, &message).await?;
-        },
+            handle_received_message(&peer_id, &id, &message, password).await?;
+        }
         SwarmEvent::NewListenAddr { address, .. } => {
             println!("Local node is listening on {}", address);
         }
@@ -185,26 +197,38 @@ async fn handle_received_message(
     peer_id: &PeerId,
     message_id: &gossipsub::MessageId,
     message: &gossipsub::Message,
+    password: &str,
 ) -> Result<(), Box<dyn Error>> {
     // Attempt to deserialize the message as FileMessage
     if let Ok(file_message) = serde_json::from_slice::<FileMessage>(&message.data) {
-        // Save the received file
-        let peer_dir = format!("./{}/", peer_id);
-        std::fs::create_dir_all(&peer_dir)?;
-        let file_path = format!("{}/{}", peer_dir, file_message.file_name);
-        std::fs::write(&file_path, &file_message.data)?;
-        println!(
-            "Received file '{}' from peer {} and saved to '{}'",
-            file_message.file_name, peer_id, file_path
-        );
+        if file_message.password == password {
+            // Save the received file
+            let peer_dir = format!("./{}/", peer_id);
+            std::fs::create_dir_all(&peer_dir)?;
+            let file_path = format!("{}/{}", peer_dir, file_message.file_name);
+            std::fs::write(&file_path, &file_message.data)?;
+            println!(
+                "Received file '{}' from peer {} and saved to '{}'",
+                file_message.file_name, peer_id, file_path
+            );
+        } else {
+            println!("Received file with invalid password from peer: {}", peer_id);
+        }
     } else {
         // Treat as a regular text message
-        println!(
-            "Got message: '{}' with id: {} from peer: {}",
-            String::from_utf8_lossy(&message.data),
-            message_id,
-            peer_id
-        );
+        let message_content = String::from_utf8_lossy(&message.data);
+        if message_content.starts_with(password) {
+            let actual_message = &message_content[password.len() + 1..]; // Skip the password and the colon
+            println!(
+                "Got message: '{}' with id: {} from peer: {}",
+                actual_message, message_id, peer_id
+            );
+        } else {
+            println!(
+                "Received message with invalid password from peer: {}",
+                peer_id
+            );
+        }
     }
     Ok(())
 }
@@ -212,6 +236,7 @@ async fn handle_received_message(
 #[derive(serde::Serialize, serde::Deserialize)]
 struct FileMessage {
     sender: String,
+    password: String,
     file_name: String,
     data: Vec<u8>,
 }
